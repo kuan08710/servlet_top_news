06512c30020c86a2a1fec745e93b3e2edad20b1f
kuan08710 <kkuan1020@gmail.com>
2024-06-29 20:50:48 +0800

    配置 filter

---
+++ top_news/src/com/louis/top_news/filters/CrosFilter.java
@@ -0,0 +1,29 @@
+package com.louis.top_news.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CrosFilter implements Filter {
+    @Override
+    public void doFilter (
+            ServletRequest servletRequest , ServletResponse servletResponse , FilterChain filterChain
+    ) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        response.setHeader("Access-Control-Allow-Origin" , "*");
+        response.setHeader("Access-Control-Allow-Methods" , "POST, GET, OPTIONS, DELETE, HEAD");
+        response.setHeader("Access-Control-Max-Age" , "3600");
+        response.setHeader("Access-Control-Allow-Headers" ,
+                           "access-control-allow-origin, authority, content-type, version-info, X-Requested-With");
+
+        // 非預先檢查請求，直接放行
+        // 預先檢查，則到此結束，不須放行
+        if (!request.getMethod()
+                    .equalsIgnoreCase("OPTIONS")) {
+            filterChain.doFilter(servletRequest , servletResponse);
+        }
+    }
+}
